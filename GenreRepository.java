b071f6742c991688c2e728d82dbbc469cdbd748d
Chanchal256 <112606244+Chanchal256@users.noreply.github.com>
2022-12-01 20:01:24 +0530

    Add files via upload

---
+++ GenreRepository.java
@@ -0,0 +1,23 @@
+package com.spring.boot.demo.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.boot.demo.model.Genre;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre,Long> {
+
+	public Genre findByTitle(String name);
+
+	Optional<Genre> findById(Long id);
+
+	List<Genre> findAll();
+
+	void deleteById(Long id);
+
+	public Genre findByName(String string);
+}
---
+++ SpringBootAppDemo002Application.java
@@ -0,0 +1,25 @@
+package com.spring.boot.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.spring.boot.demo.Controller.ActorMovieGenreController;
+
+@SpringBootApplication
+public class SpringBootAppDemo002Application implements CommandLineRunner {
+
+	@Autowired
+	private ActorMovieGenreController actorMovieGenreController;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppDemo002Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		actorMovieGenreController.run();
+
+	}
+}
